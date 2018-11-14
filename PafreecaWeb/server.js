1a05cc7292a1966f3dc6d0776b9e02685361ed90
slifspo <hhsw1606@naver.com>
2018-11-14 15:22:37 +0900

    link web server to db

--- PafreecaWeb/server.js
+++ PafreecaWeb/server.js
@@ -1,11 +1,31 @@
 ﻿const express = require("express")
+const app = express()
+
 const fs = require("fs")
 
-const app = express()
+
+const mysql = require('mysql')
 
 
 app.use(express.static('public'))
 
+var client = mysql.createConnection({
+    host: '127.0.0.1',
+    port: 3306,
+    user: 'root',
+    password: 'zxsw6714',
+    database: 'webdb'
+})
+
+client.connect();
+client.query('select * from account', [
+    ], function (errer, data) {
+        console.log(data);
+})
+
+client.query('insert into account value(1234,1234,1234,1234,0)', function (error, data) {
+    console.log('done')
+})
 
 app.get('/', (request, response) => {
 
